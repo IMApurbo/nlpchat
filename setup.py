1802718c418478b02ba249f40c0a7291d52af9e2
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-09-20 15:46:12 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="nlpchat",
-    version="0.1",
+    name='nlpchat',
+    version='0.1.0',
+    description='A package to simplify chatbot creation using NLP.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='AKM Korishee Apurbo',
+    author_email='bandinvisible8@gmail.com',
+    url='https://github.com/IMApurbo/nlpchat',
     packages=find_packages(),
     install_requires=[
-        "sentence-transformers",
-        "scikit-learn",
-        "numpy",
-        "pickle-mixin"
+        'sentence-transformers',
+        'scikit-learn',
+        'numpy',
+        'pickle-mixin',
     ],
-    author="AKM Korishee Apurbo",
-    description="An easy-to-use chatbot creation package with NLP intent identification.",
-    url="https://github.com/IMApurbo/nlpchat",
-    license="MIT",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
