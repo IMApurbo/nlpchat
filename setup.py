16a0b0f4a590b3971aefa023d4dc96963565ea5f
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-10-16 22:40:01 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nlpchat',
-    version='1.2',
+    version='2.0',
     description='A package to simplify chatbot creation using NLP.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
