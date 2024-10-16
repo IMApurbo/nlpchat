f7970ad4a57e96da38738613829fc0060cbb0f50
Akm Korishee Apurbo <155003446+IMApurbo@users.noreply.github.com>
2024-10-16 22:39:45 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'sentence-transformers',
-        'tensorflow',
         'scikit-learn',
         'numpy',
         'pickle-mixin',
